0fb3b5ebf4d20937ac78202c23c2d1e4ffb8509c
Egzona <xona.grashtica@hotmail.com>
2020-05-27 18:22:51 +0200

    Implementimi i hamburger menu
    Ne mainActivity kemi implemetuar navigimin me fragmente permes menus, si dhe dizainimi i UI tek SignUpActivity.

--- hitchhikerK/app/src/main/java/com/fiek/hitchhikerkosova/MainActivity.java
+++ hitchhikerK/app/src/main/java/com/fiek/hitchhikerkosova/MainActivity.java
@@ -1,14 +1,110 @@
 package com.fiek.hitchhikerkosova;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    Fragment fragment = null;
+    Class fragmentClass;
+    FragmentManager fragmentManager;
+
+    // Make sure to be using androidx.appcompat.app.ActionBarDrawerToggle version.
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //E nderrojm ActionBar me ToolBar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
+
+        //Ne create loadohet fragmenti 1
+        fragmentClass = TestFragment1.class;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fContent, fragment).commit();
+
+        //Listeneri per me u qel hamburger menu
+        drawerToggle = new ActionBarDrawerToggle(this, mDrawer  , toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerToggle.setDrawerIndicatorEnabled(false);
+        drawerToggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDrawer.openDrawer(GravityCompat.START);
+            }
+        });
+        drawerToggle.setHomeAsUpIndicator(R.drawable.ic_hamburger);
     }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Navigimi neper fragmente
+
+        switch(menuItem.getItemId()) {
+            case R.id.nav_first_fragment:
+                fragmentClass = TestFragment1.class;
+                break;
+            case R.id.nav_second_fragment:
+                fragmentClass = TestFragment2.class;
+                break;
+
+            default:
+                fragmentClass = TestFragment1.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fContent, fragment).commit();
+
+
+        menuItem.setChecked(true);
+         setTitle(menuItem.getTitle());
+        // Mbyllja e menus
+        mDrawer.closeDrawers();
+    }
+
+
 }
